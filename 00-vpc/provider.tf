7fa9aa58786f7a14bc38b09d9af5ba06b80daf93
prasad <prasad@gmail.com>
2025-02-09 16:49:36 +0530

    ecom-infra

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "ecom-dev"
+    key    = "ecom-vpc"
+    region = "us-east-1"
+    dynamodb_table = "ecom-locking-dev"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
